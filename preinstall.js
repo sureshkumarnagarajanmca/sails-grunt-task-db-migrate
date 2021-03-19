967f36547c089a23e2c9c3360df8aeb07d57a55c
Sureshkumar Nagarajan <suresh@niyati.com>
2021-03-19 20:37:56 +0530

    Correction.

--- preinstall.js
+++ preinstall.js
@@ -1,39 +1,25 @@
 const util = require( 'util' )
 const exec = util.promisify( require( 'child_process' ).exec )
-const fs = require( 'fs' ).promises
+
 let packageFilePath = '../../package.json'
 let package = require( packageFilePath )
 let packageString = JSON.stringify( package )
 
 let fun = ( async () => {
-  let packagesToBeInstalledArray = [ "db-migrate" ]
+  let command = `npm i -S db-migrate`
 
   if( packageString.indexOf( '"sails-mongo":' ) !== -1 ) {
-    packagesToBeInstalledArray.push( "db-migrate-mongodb" )
+    command += ` db-migrate-mongodb `
   } else if( packageString.indexOf( '"sails-mysql":' ) !== -1 ) {
-    packagesToBeInstalledArray.push( "db-migrate-mysql" )
+    command += ` db-migrate-mysql `
   } else if( packageString.indexOf( '"sails-postgresql":' ) !== -1 ) {
-    packagesToBeInstalledArray.push( "db-migrate-pg" )
+    command += ` db-migrate-pg `
   }
 
-  let packagesToBeInstalledObj = {}
+  command += `--prefix ../../ ../../`
 
   try {
-    let result = await exec( `npm view ${ packagesToBeInstalledArray[ 0 ] } version` )
-    packagesToBeInstalledObj[ packagesToBeInstalledArray[ 0 ] ] = "^" + result.stdout.replace(/\n/g, '')
-
-    if( typeof packagesToBeInstalledArray[ 1 ] !== 'undefined' ) {
-      result = await exec( `npm view ${ packagesToBeInstalledArray[ 1 ] } version` )
-      packagesToBeInstalledObj[ packagesToBeInstalledArray[ 1 ] ] = "^" + result.stdout.replace(/\n/g, '')
-    }
-
-    package.dependencies = {
-      ...packagesToBeInstalledObj, 
-      ...package.dependencies  
-    }
-
-    fs.writeFile( packageFilePath, JSON.stringify( package, null, 2 ) )
-    
+    await exec( command )
   } catch( e ) {
     throw e
   }
