cae6692370248a53ee08def94ec207c263ce1f11
Sureshkumar Nagarajan <suresh@niyati.com>
2021-03-19 19:54:58 +0530

    Correction.

--- postinstall.js
+++ postinstall.js
@@ -3,7 +3,13 @@ const exec = util.promisify( require( 'child_process' ).exec )
 
 let fun = ( async () => {
   try {
-    let result = await exec( `npm i ../../ --prefix ../../` )
+    try {
+      let checkPackageInstalled = require( 'gulp' )
+
+      return
+    } catch( e ) {
+      let result = await exec( `npm i ../../ --prefix ../../` )
+    }
   } catch( e ) {
     throw e
   }
