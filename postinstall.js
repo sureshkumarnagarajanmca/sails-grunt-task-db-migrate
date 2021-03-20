68da750fa8325b0188e1858e7f2d28b88fc3e784
Sureshkumar Nagarajan <suresh@niyati.com>
2021-03-20 09:04:04 +0530

    Correction.

--- postinstall.js
+++ postinstall.js
@@ -1,25 +1,14 @@
 const util = require( 'util' )
 const exec = util.promisify( require( 'child_process' ).exec )
-
-let packageFilePath = '../../package.json'
-let package = require( packageFilePath )
-let packageString = JSON.stringify( package )
+const fs = require( 'fs' )
 
 let fun = ( async () => {
-  let command = `npm i -S db-migrate`
-
-  if( packageString.indexOf( '"sails-mongo":' ) !== -1 ) {
-    command += ` db-migrate-mongodb `
-  } else if( packageString.indexOf( '"sails-mysql":' ) !== -1 ) {
-    command += ` db-migrate-mysql `
-  } else if( packageString.indexOf( '"sails-postgresql":' ) !== -1 ) {
-    command += ` db-migrate-pg `
-  }
-
-  command += `--prefix ../../ ../../`
-
   try {
-    await exec( command )
+    if( fs.existsSync( '../../node_modules/db-migrate' ) ) {
+      return
+    }
+
+    let result = await exec( `npm i ../../ --prefix ../../` )
   } catch( e ) {
     throw e
   }
