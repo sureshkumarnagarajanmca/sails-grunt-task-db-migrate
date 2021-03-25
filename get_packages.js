c73a3eaa8056bdb1e1db00a4db0c871624c9efd4
Sureshkumar Nagarajan <suresh@niyati.com>
2021-03-25 12:09:01 +0530

    Correction.

--- get_packages.js
+++ get_packages.js
@@ -1,14 +1,28 @@
 let packageFilePath = '../../package.json'
 let package = require( packageFilePath )
 let packageString = JSON.stringify( package )
-let sails = require( 'sails' )
+var Sails = require('sails').Sails
 
 let packagesToBeInstalledArray = [ "db-migrate" ]
 
 let getPackages = () => {
-  let sailsConfig = sails.config
+  let env = process.env.NODE_ENV
+
+  let sailsConfig = {
+    port: -1,
+    log: { level: process.env.LOG_LEVEL || 'silent' },
+    environment: env,
+    migrating: true
+  };
+
+  // load Sails to get the effective configuration. We don't actually need to
+  // run it, and we certainly don't want any log messages. We just want the
+  // config.
+  sails = new Sails()
+  sailsConfig = sails.config
+  console.log( sailsConfig )
   let connection
-console.log( sailsConfig )
+
   if (!sailsConfig.migrations) {
     throw new Error('Migrations not configured. Please setup ./config/migrations.js')
   }
